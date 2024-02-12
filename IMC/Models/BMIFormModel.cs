07b38b9d5ac8ca5681634ee99142addcb9c18045
Eliane Henriqueta <elianehenriqueta@gmail.com>
2024-02-12 17:40:25 -0300

    Craindo os card e alogica do calculo do icm

---
+++ IMC/Models/BMIFormModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace IMC.Models
+{
+    //    public class BMIFormModel
+    //    {
+    //        [Required(ErrorMessage = "A altura é obrigatória.")]
+    //        [Range(0.01,double.MaxValue, ErrorMessage = "A altura invalida.")]
+    //        public double Altura { get; set; }
+
+    //        [Required(ErrorMessage = "O peso é obrigatório.")]
+    //        [Range(1.0, double.MaxValue, ErrorMessage = "Peso invalido.")]
+    //        public double Peso { get; set; }
+
+    //        [Required(ErrorMessage = "O sexo é obrigatório.")]
+    //        public string Sexo { get; set; }
+
+
+    //        public bool Mais65Anos { get; set; }
+    //    }
+    //}
+    public class BMIFormModel
+    {
+        [Required(ErrorMessage = "A altura é obrigatória.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "A altura é inválida.")]
+        public string Altura { get; set; }
+
+        [Required(ErrorMessage = "O peso é obrigatório.")]
+        [Range(1.0, double.MaxValue, ErrorMessage = "O peso é inválido.")]
+        public string Peso { get; set; }
+
+        [Required(ErrorMessage = "O sexo é obrigatório.")]
+        public string Sexo { get; set; }
+
+        public bool Mais65Anos { get; set; }
+    }
+}
